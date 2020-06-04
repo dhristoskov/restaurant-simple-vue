232724476a0240967c8f85c689b866ace854edce
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-04 11:25:27 +0200

    commit events

---
+++ src/Events/EventsPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="offers-page">
+        <header>
+            <header-main />
+        </header>
+        <h2>Offers Page</h2>
+    </div>
+</template>
+
+<script>
+import HeaderMain from '../shared-components/Header/HeaderMain';
+
+export default {
+    components: {
+        headerMain: HeaderMain
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.offers-page {
+        header {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+        }   
+}
+</style>

035fdb625badc169a0491dfaf3a9450b282d737b
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-05 12:38:50 +0200

    commit

---
+++ src/shared-components/Modal/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <backdrop :hideModal='hideModal'/>
+        <div class="modal">
+            <p @click='hideModal'>close</p>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Backdrop from '../BackDrop/BackDrop';
+    export default {
+        props: ['hideModal'],
+        components:{
+            backdrop: Backdrop
+        }
+}
+</script>
+
+<style lang="scss" scoped>
+    .modal {
+        position: fixed;
+        z-index: 40;
+        background-color: white;
+        width: 30%;
+        border: 1px solid black;
+        box-shadow: 1px 1px 1px black;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        p {
+            position: fixed;
+            top: -25px;
+            right: -5px;
+            color: black;
+            text-align: center;
+            font-size: 12px;
+            font-weight: 700;
+            text-transform: uppercase;
+            padding: 2px 4px;
+            width: 50px;
+            margin-left: auto;
+            cursor: pointer;
+        }
+    }
+</style>

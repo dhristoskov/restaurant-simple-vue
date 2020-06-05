8d6bec658067e03b71e30f0a3a9c650777c75952
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-05 12:37:35 +0200

    commit menu

---
+++ src/Menu/MenuPage.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="menu-page">
+        <header>
+            <header-main />
+        </header>
+        <img src='../assets/cafe.jpg' alt="cafe-img">
+        <ul>
+            <item v-for="item in menu" :item='item' :key='item.id'/>
+        </ul>
+    </div>
+</template>
+
+<script>
+import MenuItem from './componenrs/MenuItem';
+import HeaderMain from '../shared-components/Header/HeaderMain';
+import { menu } from '../shared-components/data';
+
+export default {
+    data() {
+        return {
+            menu: menu
+        }
+    },
+    components: {
+        item: MenuItem,
+        headerMain: HeaderMain,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .menu-page {
+        display: flex;
+        height: 100vh;  
+        justify-content: flex-start;  
+        header {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+        }   
+        img {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+        }
+        ul {
+            background-color:rgba($color: #FFF, $alpha: .45);
+            flex: 0 0 50%;
+            padding-top: 150px;
+            text-align: center;
+        }
+    }
+</style>
---
+++ src/Menu/componenrs/MenuItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p class="name">{{item.name}}<span>{{item.price}} €</span></p>
+        <p class="info">{{item.note}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [ 'item' ]
+    }
+</script>
+
+<style scoped lang="scss">
+    .name {
+        color: rgb(8, 19, 99);
+        font-weight: 700;
+        font-size: 18px;
+        margin-bottom: 3px;
+        span {
+            margin-left: 7px;
+            font-weight: 500;
+             font-size: 15px;           
+        }
+    }
+    .info {
+         margin-bottom: 15px;
+         font-weight: 600;
+    }
+</style>

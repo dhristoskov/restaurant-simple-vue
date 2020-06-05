1cdecf6a07150fa4fae59202b0e46b19f0515b85
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-05 12:39:24 +0200

    commit

---
+++ src/shared-components/BackDrop/BackDrop.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="backdrop"  @click='hideModal'>
+    </div>
+</template>
+
+<script>
+export default {
+     props: ['hideModal']
+}
+</script>
+
+<style lang="scss" scoped>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: rgba(153, 154, 155, 0.85);
+        z-index: 30;
+    }
+</style>

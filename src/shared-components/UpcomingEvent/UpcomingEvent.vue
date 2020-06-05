c83b046d6bbcafeeaaa935a61c0f13a98b255ea4
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-05 12:38:17 +0200

    commit

---
+++ src/shared-components/UpcomingEvent/UpcomingEvent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="event-container">
+        <p>Tomorrow from 12:00 to 18:00</p>
+        <p>Our Cakes are 50% off</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .event-container {
+        width: auto;
+        height: 550px;
+        background-image: url(../../assets/cake.jpg);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        p {
+            text-transform: uppercase;
+            font-size: 22px;
+            font-weight: 700;
+            color: rgb(241, 250, 117);
+        }
+    }
+</style>
---
+++ src/shared-components/data.js
@@ -0,0 +1,95 @@
+export const menu = [
+    {
+        id: 1,
+        name: 'Esspresso',
+        note: 'Lavazza, Illy or Sagafredo',
+        price: 2.99
+    },
+     {
+        id: 2,
+        name: 'Cafe Latte',
+        note: 'Our best cafe with milk on your choice',
+        price: 3.59
+    },
+     {
+        id: 3,
+        name: 'Cappuccino',
+        note: 'Our best cafe prepared with love',
+        price: 3.99
+    },
+    {
+        id: 4,
+        name: 'Latte Macchiato',
+        note: 'The best Macchiato in town',
+        price: 2.29
+    },
+     {
+        id: 5,
+        name: 'Tea',
+        note: 'Best Tea selection for you',
+        price: 1.59
+    },
+    {
+        id: 6,
+        name: 'Orange Juice',
+        note: 'Prepared with fresh oranges',
+        price: 2.99
+    },
+    {
+        id: 7,
+        name: 'Water',
+        note: 'Variety of water cold or hot',
+        price: 1.59
+    }
+];
+
+export const events = [
+    {
+        id: 1,
+        day: 'Monday',
+        first_offer: {
+            product: 'Black Forest',
+            price: 1.29
+        },
+        second_offer: {
+            product: 'Doughnut',
+            price: 0.39
+        }
+    },
+    {
+        id: 2,
+        day: 'Tuesday',
+        first_offer: {
+            product: 'Cheesecake',
+            price: 1.59
+        },
+        second_offer: {
+            product: 'Ice Cream',
+            price: 0.59
+        }
+    },
+    {
+        id: 3,
+        day: 'Wednesday',
+        first_offer: {
+            product: 'Cupcake ',
+            price: 1.59
+        },
+        second_offer: {
+            product: 'Apple pie',
+            price: 0.99
+        }
+    },
+    {
+        id: 4,
+        day: 'Thursday',
+        first_offer: {
+            product: 'Lemon cake',
+            price: 1.59
+        },
+        second_offer: {
+            product: 'Eclair',
+            price: 0.99
+        }
+    }
+]
